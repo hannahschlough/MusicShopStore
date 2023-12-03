02bda5db2c64d1c851a11051e8d1ebd1aa7ee007
hannahschlough <hannah.schlough@ndsu.edu>
2023-12-03 13:36:22 -0600

    added success page

---
+++ Controllers/Success.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MusicShopStore.Controllers
+{
+    public class Success : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
