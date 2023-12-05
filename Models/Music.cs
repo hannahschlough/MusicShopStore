e5a16e402dc65889dc62a18952ab3067cfabc57a
graciegorka <gracie82003@gmail.com>
2023-12-05 11:48:46 -0600

    Added dropboxes for user to filter music page

--- Models/Music.cs
+++ Models/Music.cs
@@ -1,19 +1,36 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
 namespace MusicShopStore.Models
 {
     public class Music
     {
         public required int MusicId { get; set; }
 
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+        [Required]
+        [StringLength(30)]
         public required string Title { get; set; }
 
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+        [Required]
+        [StringLength(30)]
         public required string Artist { get; set; }
 
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+        [Required]
+        [StringLength(30)]
         public required string Genre { get; set; }
 
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+        [Required]
+        [StringLength(30)]
         public required string Type { get; set; }
 
+        [Range(1, 100)]
+        [DataType(DataType.Currency)]
         public required decimal Price { get; set; }
+
+     
 
     }
 }
